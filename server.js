d9158f9caf6426f1ceec27aad793c048a9d35544
shubhamdhiman <shubhamdhiman00.sd@gmail.com>
2023-07-10 19:29:54 +0530

    env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Importing Mongoose Connection and calling it
 const connectDB = require('./config/apidb')
