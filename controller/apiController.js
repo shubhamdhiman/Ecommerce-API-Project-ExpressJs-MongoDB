266de7adf4e8fb94116c42500d742a510bd863a3
shubhamdhiman <shubhamdhiman00.sd@gmail.com>
2023-07-10 20:09:12 +0530

    fixed bug

--- controller/apiController.js
+++ controller/apiController.js
@@ -33,7 +33,7 @@ const updateProduct = asyncHandler(async function (req, res) {
   const update = await apiSchema.findByIdAndUpdate(req.params.id, {
     quantity: req.params.update_quantity,
   });
-  const data = await apiSchema.findOne(req.res.id);
+  const data = await apiSchema.findOne(req.params.id);
   res.send({ data, message: "Product Updated" });
 });
 
